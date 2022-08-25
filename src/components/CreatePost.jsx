39c2a938864fa5bdfb18c478e796566e7e71a72d
unknown <hetarth002@gmail.com>
2022-08-25 23:20:40 +0530

    add route

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [post, setPost] = useState("");
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangePost = (e) => {
+    setPost(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const blogPost = {
+      title: title,
+      post: post,
+    };
+    console.log(blogPost);
+    axios("http://localhost:5000/add", blogPost)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
   return (
-    <form action="/" method="post" className="container">
+    <form onSubmit={onSubmit} className="container">
       <div className="mb-3">
         <label className="form-label">Title</label>
-        <input class="form-control"></input>
+        <input onChange={onChangeTitle} className="form-control"></input>
       </div>
       <div className="mb-3">
         <label className="form-label">Write Post</label>
-        <textarea className="form-control" rows="15"></textarea>
+        <textarea
+          onChange={onChangePost}
+          className="form-control"
+          rows="15"
+        ></textarea>
       </div>
       <button type="submit">POST</button>
     </form>
